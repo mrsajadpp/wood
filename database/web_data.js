c7b3f5ad42c8c74ca6f5cdb5260b43e6450e0058
mrsajadpp <saju4753@gmail.com>
2023-02-19 10:19:00 +0530

    index bug fixes

--- database/web_data.js
+++ database/web_data.js
@@ -6,13 +6,15 @@ module.exports = {
     addIndex: (data) => {
         try {
             return new Promise(async (resolve, reject) => {
-                let index = await db.get().collection(COLLECTIONS.INDEX).findOne({ url: data.url })
-                if (!index) {
-                    db.get().collection(COLLECTIONS.INDEX).insertOne(data).then((res) => {
-                        resolve({ status: false })
-                    })
-                } else {
-                    reject({ status: true })
+                if (data.url && data.title && data.description) {
+                    let index = await db.get().collection(COLLECTIONS.INDEX).findOne({ url: data.url })
+                    if (!index) {
+                        db.get().collection(COLLECTIONS.INDEX).insertOne(data).then((res) => {
+                            resolve({ status: false })
+                        })
+                    } else {
+                        reject({ status: true })
+                    }
                 }
             })
         } catch (err) {
