46d500ff6b26ae35efff622407cb5091a2b382c9
mrsajadpp <saju4753@gmail.com>
2023-02-19 16:51:33 +0530

    speed increases

--- modules/modules.js
+++ modules/modules.js
@@ -9,24 +9,22 @@ module.exports = {
     addIndex: (origin, pathname) => {
         return new Promise(async (resolve, reject) => {
             try {
-                let url = new URL(origin + pathname);
-                request(url.href, (error, response, body) => {
-                    if (error) {
-                        console.error(`Error crawling ${url.href}: ${error}`);
-                        return;
-                    }
-                    // Load HTML content into Cheerio
-                    const $ = cheerio.load(body);
-                    let data = {
-                        "url": url.href,
-                        "origin": url.origin,
-                        "protocol": url.protocol,
-                        "title": $('title').text(),
-                        "description": $('meta[name="description"]').attr('content'),
-                        "links": $('a').map((i, el) => $(el).attr('href')).get()
-                    }
-                    webData.addIndex(data).then((response) => { }).catch((err) => {})
-                })
+                    let url = new URL(origin + pathname);
+                    request(url.href, (error, response, body) => {
+                        if (!error && response.statusCode == 200) {
+                            // Load HTML content into Cheerio
+                            const $ = cheerio.load(body);
+                            let data = {
+                                "url": url.href,
+                                "origin": url.origin,
+                                "protocol": url.protocol,
+                                "title": $('title').text(),
+                                "description": $('meta[name="description"]').attr('content'),
+                                "links": $('a').map((i, el) => $(el).attr('href')).get()
+                            }
+                            webData.addIndex(data).then((res) => { }).catch((err) => { })
+                        }
+                    })
             } catch (err) {
                 console.error(err)
             }
--- routes/index.js
+++ routes/index.js
@@ -73,32 +73,31 @@ router.post('/index', (req, res, next) => {
       const origin = url.origin;
       // Send HTTP request to URL
       request(url.href, (error, response, body) => {
-        if (error) {
-          console.error(`Error crawling ${url.href}: ${error}`);
-          res.render('index', { title: 'Index your pages in wood', description: 'Index your pages in wood', style: 'search', status: true, err: "Sorry facing a error in indexing this page." })
-          return;
-        }
-        // Load HTML content into Cheerio
-        const $ = cheerio.load(body);
-        let data = {
-          "url": url.href,
-          "origin": url.origin,
-          "protocol": url.protocol,
-          "title": $('title').text(),
-          "description": $('meta[name="description"]').attr('content'),
-          "links": $('a').map((i, el) => $(el).attr('href')).get()
-        }
-        webData.addIndex(data).then((response) => {
-          res.redirect('/index')
-        }).catch((err) => {
-          res.render('index', { title: 'Index your pages in wood', description: 'Index your pages in wood', style: 'search', status: true, err: "Sorry this page is already indexed you cant index this page." })
-        })
-        for (let i = 0; i < data.links.length; i++) {
-          if (data.links[i].startsWith('https://') || data.links[i].startsWith('http://')) {
-            modules.addIndex(origin, new URL(data.links[i]).pathname).then((stat) => { }).catch((err) => { })
-          } else {
-            modules.addIndex(origin, data.links[i]).then((stat) => { }).catch((err) => { })
+        if (!error && response.statusCode == 200) {
+          // Load HTML content into Cheerio
+          const $ = cheerio.load(body);
+          let data = {
+            "url": url.href,
+            "origin": url.origin,
+            "protocol": url.protocol,
+            "title": $('title').text(),
+            "description": $('meta[name="description"]').attr('content'),
+            "links": $('a').map((i, el) => $(el).attr('href')).get()
           }
+          webData.addIndex(data).then((response) => {
+            res.redirect('/index')
+            for (let i = 0; i < data.links.length; i++) {
+              if (data.links[i].startsWith('https://') || data.links[i].startsWith('http://')) {
+                modules.addIndex(origin, new URL(data.links[i]).pathname)
+              } else {
+                modules.addIndex(origin, data.links[i])
+              }
+            }
+          }).catch((err) => {
+            res.render('index', { title: 'Index your pages in wood', description: 'Index your pages in wood', style: 'search', status: true, err: "Sorry this page is already indexed you cant index this page." })
+          })
+        } else {
+          res.render('index', { title: 'Index your pages in wood', description: 'Index your pages in wood', style: 'search', status: true, err: "Sorry this page is not exist." })
         }
       });
     }
